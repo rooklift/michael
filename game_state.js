2240b82d5bb6b185158c1997ab21147f548de50a
rooklift <16438795+rooklift@users.noreply.github.com>
2021-06-28 17:48:05 +0100

    move parser to game_state.js

--- game_state.js
+++ game_state.js
@@ -38,7 +38,9 @@ let game_state_props = {
 	},
 
 	string: function() {
+
 		let lines = ["-".repeat(this.width)];
+
 		for (let y = 0; y < this.height; y++) {
 			let line = [];
 			for (let x = 0; x < this.width; x++) {
@@ -46,8 +48,82 @@ let game_state_props = {
 			}
 			lines.push(line.join(""));
 		}
+
 		lines.push("-".repeat(this.width));
 		return lines.join("\n");
+	},
+
+	parse: function(fields) {
+
+		if (fields[0] === "c") {				// c t city_id f lk
+
+			let team = parseInt(fields[1], 10);
+			let id = fields[2];
+			let fuel = parseInt(fields[3], 10);
+			let lk = parseInt(fields[4], 10);
+
+			this.cities.push({team, id, fuel, lk});
+			return;
+		}
+
+		if (fields[0] === "ccd") {				// ccd x y cd
+
+			let x = parseInt(fields[1], 10);
+			let y = parseInt(fields[2], 10);
+			let cd = parseInt(fields[3], 10);
+
+			this.map[x][y].cd = cd;
+			return;
+		}
+
+		if (fields[0] === "ct") {				// ct t city_id x y cd
+
+			let team = parseInt(fields[1], 10);
+			let id = fields[2];
+			let x = parseInt(fields[3], 10);
+			let y = parseInt(fields[4], 10);
+			let cd = parseInt(fields[5], 10);
+
+			this.tiles.push({team, id, x, y, cd});
+			return;
+		}
+
+		if (fields[0] === "r") {				// r resource_type x y amount
+
+			let type = fields[1][0];
+			let x = parseInt(fields[2], 10);
+			let y = parseInt(fields[3], 10);
+			let amount = parseInt(fields[4], 10);
+
+			this.map[x][y].type = type;
+			this.map[x][y].amount = amount;
+			return;
+		}
+
+		if (fields[0] === "rp") {				// rp t points
+
+			let team = parseInt(fields[1], 10);
+			let points = parseInt(fields[2], 10);
+
+			this.rp[team] = points;
+			return;
+		}
+
+		if (fields[0] === "u") {				// u unit_type t unit_id x y cd w c u
+
+			let type = parseInt(fields[1], 10);
+			let team = parseInt(fields[2], 10);
+			let id = fields[3];
+			let x = parseInt(fields[4], 10);
+			let y = parseInt(fields[5], 10);
+			let cd = parseInt(fields[6], 10);
+			let wood = parseInt(fields[7], 10);
+			let coal = parseInt(fields[8], 10);
+			let uranium = parseInt(fields[9], 10);
+
+			this.units.push({type, team, id, x, y, cd, wood, coal, uranium});
+			return;
+		}
 	},
 
 };
--- main.js
+++ main.js
@@ -6,10 +6,7 @@ const readline = require("readline");
 const new_game_state = require("./game_state");
 const ai = require("./ai");
 
-const LOG = false;
-const LOG_INPUT = false;
-const LOG_OUTPUT = false;
-const LOG_STATES = false;
+const LOG = true;
 
 // ------------------------------------------------------------------------------------------------
 
@@ -46,8 +43,8 @@ let bot = {
 	// --------------------------------------------------------------------------------------------
 
 	send: function(s) {
+		// this.log("> " + s);
 		console.log(s);
-		if (LOG_OUTPUT) this.log("> " + s);
 	},
 
 	log: function(s) {
@@ -62,102 +59,32 @@ let bot = {
 
 	handle_line: function(s, lineno) {
 
-		if (LOG_INPUT) this.log("< " + s);
 		let fields = s.split(" ");
 
 		// The first 2 lines we ever receive are special...
 
 		if (lineno === 0) {
+
 			this.team = parseInt(fields[0], 10);
 			this.start_log(`michael_${this.team}.log`);
-			if (LOG_INPUT) this.log("< " + s);
-			return;
-		}
 
-		if (lineno === 1) {
+		} else if (lineno === 1) {
+
 			let width = parseInt(fields[0], 10);
 			let height = parseInt(fields[1], 10);
 			this.state = new_game_state(width, height);
-			return;
-		}
 
-		if (fields[0] === "c") {				// c t city_id f lk
+		} else if (fields[0] === "D_DONE") {
 
-			let team = parseInt(fields[1], 10);
-			let id = fields[2];
-			let fuel = parseInt(fields[3], 10);
-			let lk = parseInt(fields[4], 10);
+			// this.log(this.state.string());
 
-			this.state.cities.push({team, id, fuel, lk});
-			return;
-		}
-
-		if (fields[0] === "ccd") {				// ccd x y cd
-
-			let x = parseInt(fields[1], 10);
-			let y = parseInt(fields[2], 10);
-			let cd = parseInt(fields[3], 10);
-
-			this.state.map[x][y].cd = cd;
-			return;
-		}
-
-		if (fields[0] === "ct") {				// ct t city_id x y cd
-
-			let team = parseInt(fields[1], 10);
-			let id = fields[2];
-			let x = parseInt(fields[3], 10);
-			let y = parseInt(fields[4], 10);
-			let cd = parseInt(fields[5], 10);
-
-			this.state.tiles.push({team, id, x, y, cd});
-			return;
-		}
-
-		if (fields[0] === "r") {				// r resource_type x y amount
-
-			let type = fields[1][0];
-			let x = parseInt(fields[2], 10);
-			let y = parseInt(fields[3], 10);
-			let amount = parseInt(fields[4], 10);
-
-			this.state.map[x][y].type = type;
-			this.state.map[x][y].amount = amount;
-			return;
-		}
-
-		if (fields[0] === "rp") {				// rp t points
-
-			let team = parseInt(fields[1], 10);
-			let points = parseInt(fields[2], 10);
-
-			this.state.rp[team] = points;
-			return;
-		}
-
-		if (fields[0] === "u") {				// u unit_type t unit_id x y cd w c u
-
-			let type = parseInt(fields[1], 10);
-			let team = parseInt(fields[2], 10);
-			let id = fields[3];
-			let x = parseInt(fields[4], 10);
-			let y = parseInt(fields[5], 10);
-			let cd = parseInt(fields[6], 10);
-			let wood = parseInt(fields[7], 10);
-			let coal = parseInt(fields[8], 10);
-			let uranium = parseInt(fields[9], 10);
-
-			this.state.units.push({type, team, id, x, y, cd, wood, coal, uranium});
-			return;
-		}
-
-		if (fields[0] === "D_DONE") {
-			if (LOG_STATES) {
-				this.log(this.state.string());
-			}
 			ai(this, this.state, this.team);
 			this.state.reset();					// Must reset the map for the next turn. (?)
-			return;
+
+		} else {
+
+			this.state.parse(fields);
+
 		}
 
 	},
