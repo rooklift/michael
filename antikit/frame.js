1659aa8b1c0505703bafdcff592907e895f1cc63
rooklift <16438795+rooklift@users.noreply.github.com>
2021-07-06 09:50:45 +0100

    Update frame.js

--- antikit/frame.js
+++ antikit/frame.js
@@ -164,8 +164,8 @@ let frame_prototype = {
 		}
 	},
 
-	finish_parse() {								// At this point, we could add stuff like cached arrays of the various resources etc
-		this.__freeze_recurse(this);
+	finish_parse() {								// At this point, we could add stuff like cached arrays of the various resources etc.
+		this.__freeze_recurse(this);				// We might also want to not freeze things, adding properties to units could be useful.
 	},
 
 	__freeze_recurse(o) {		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
