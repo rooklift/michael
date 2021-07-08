d9251d9671ff5f70b7a9149c09df59a552d01d03
rooklift <16438795+rooklift@users.noreply.github.com>
2021-07-08 14:31:55 +0100

    Add city.copy()

--- antikit/city.js
+++ antikit/city.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = function(frame, team, id, fuel, upkeep) {
+function new_city(frame, team, id, fuel, upkeep) {
 	return Object.assign(Object.create(city_prototype), {frame, team, id, fuel, upkeep});
 };
 
@@ -9,6 +9,10 @@ module.exports = function(frame, team, id, fuel, upkeep) {
 let city_prototype = {
 
 	is_city: true,
+
+	copy() {
+		return new_city(this.frame, this.team, this.id, this.fuel, this.upkeep);
+	},
 
 	needy() {
 		return this.fuel < this.upkeep * 10;
@@ -19,3 +23,7 @@ let city_prototype = {
 	},
 
 };
+
+// ------------------------------------------------------------------------------------------------
+
+module.exports = new_city;
