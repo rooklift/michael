d0c4eca9c725675c8b1f4ed735d05b2cefa034aa
rooklift <16438795+rooklift@users.noreply.github.com>
2021-07-22 02:55:13 +0100

    single line output

--- antikit/frame.js
+++ antikit/frame.js
@@ -209,21 +209,26 @@ let frame_prototype = {
 
 	send_orders() {
 
+		let commands = [];
+
 		for (let unit of this.units) {
-			unit.transmit();
+			commands.push(unit.get_command());
 		}
 
 		for (let house of this.houses) {
-			house.transmit();
+			commands.push(house.get_command());
 		}
 
 		for (let x = 0; x < this.width; x++) {
 			for (let y = 0; y < this.height; y++) {
-				this.map[x][y].transmit();
+				commands.push(this.map[x][y].get_command());		// For annotations (if we have any).
 			}
 		}
 
-		console.log("D_FINISH");
+		commands = commands.filter(s => s !== "").join(",");
+
+		console.log(commands);
+		// console.log("D_FINISH");
 	},
 
 };
